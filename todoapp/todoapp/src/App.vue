9afe1792c996db61bf708d317fcd6531a2061da2
Ahmet <ahmetzumber5@gmail.com>
2021-09-07 19:52:05 +0300

    view design handled

--- todoapp/todoapp/src/App.vue
+++ todoapp/todoapp/src/App.vue
@@ -1,40 +1,75 @@
 <template>
   <div class="container">
-    <Header title="To Do App"/>
-    <Button title="Add To Do"/>
+    <h3>To Do App</h3>
+    <div class="input-group">
+      <input v-model="inputTodo" type="text" class="form-control"/>
+      <div class="input-group-append">
+        <button @click="addToDo" class="btn btn-primary" type="button">Submit</button>
+      </div>
+    </div>
+    <br>
+    <ul class="list-group">
+        <li :v-for="todo in todos" class="list-group-item d-flex justify-content-between align-items-center">
+          {{ todo.text }}
+          <button id="remove-btn" class="badge badge-primary badge-pill">X</button>
+        </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
-import Button from './components/Button.vue'
-
 export default {
-  name: 'App',
-  components: {
-    Header,
-    Button
+  name: "App",
+  components: {}, 
+  data(){
+    return{
+      todos: [
+        { text: 'Learn JavaScript' },
+        { text: 'Learn Vue' },
+        { text: 'Build something awesome' }
+      ]
+    }
+  },
+  methods:{
+    addToDo(){
+      if(this.inputTodo !== ""){
+        this.todos.push({ text: this.inputTodo })
+        this.inputTodo = ""
+      }
+    }
   }
-}
+};
 </script>
 
 <style>
-  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
-  body {
-    font-family: 'Poppins', sans-serif;
-  }
-  .container {
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap");
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  background: lightsteelblue;
+}
+body {
+  font-family: "Poppins", sans-serif;
+}
+.container {
   max-width: 500px;
   margin: 30px auto;
   overflow: auto;
   min-height: 300px;
-  border: 1px solid steelblue;
+  background: aliceblue;
+  box-shadow: 1px 1px 3px 1px rgba(0.2, 0.2, 0.2, 0.2);
   padding: 30px;
   border-radius: 5px;
+}
+h3 {
+  background: aliceblue;
+}
+.body {
+  background: aliceblue;
+}
+#remove-btn{
+  border: none;
+  background: black;
 }
 </style>
