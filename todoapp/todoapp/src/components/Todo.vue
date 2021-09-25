1d359bfce24354649e9b4f84cba7a976769947cf
Ahmet <ahmetzumber5@gmail.com>
2021-09-25 23:59:09 +0300

    form group created

---
+++ todoapp/todoapp/src/components/Todo.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="formm">
+        <input v-model="inputTodo" class="form-control" type="text" v-on:keyup.enter="onInput">
+        <button class="btn btn-primary" @click="add">Add</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Todo",
+    data(){
+        return{
+            inputTodo: "",
+        }
+    },
+    methods:{
+        add(){
+            this.$emit('add',this.inputTodo)
+        }
+    }
+}
+</script>
+
+<style >
+    .formm {
+        display: flex;
+        align-items: center;
+    }
+    button{
+        margin: 10px; 
+        margin-bottom: 15px
+    }
+</style>
