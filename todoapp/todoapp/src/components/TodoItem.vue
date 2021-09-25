89bb480d47c3482ab659aba2deeff65cfa056965
Ahmet <ahmetzumber5@gmail.com>
2021-09-25 23:59:51 +0300

    todo items added

---
+++ todoapp/todoapp/src/components/TodoItem.vue
@@ -0,0 +1,17 @@
+<template>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            {{ todoText }}
+            <button class="btn btn-sm btn-danger btn-pill">X</button>
+        </li>
+</template>
+
+<script>
+export default {
+    name:'TodoItem',
+    props: ['todoText'],
+}
+</script>
+
+<style scoped>
+
+</style>
