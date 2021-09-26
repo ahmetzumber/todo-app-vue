54a04e79e195e53887c8aa2b20454ac11ea3e290
Ahmet <ahmetzumber5@gmail.com>
2021-09-26 11:41:45 +0300

    styles changed

--- todoapp/todoapp/src/App.vue
+++ todoapp/todoapp/src/App.vue
@@ -2,7 +2,7 @@
   <div id="cont" class="container" style="margin-top: 20px:">
       <h2>To Do App</h2>
       <Todo @add="addTodo"/>  
-      <TodoItem v-for="todo in todos" :key="todo.id" :todoText="todo.text"/>     
+      <TodoItem v-for="todo in todos" :key="todo.id" :todoText="todo.text" :todoId="todo.id" @remove="removeTodo(todo.id)"/>     
   </div>
 </template>
 
@@ -30,6 +30,9 @@ export default {
       if(this.input != null){
         this.todos.push({ id: ++this.count, text: this.input })
       }
+    },
+    removeTodo(removedItem){
+      this.todos = this.todos.filter(todo => todo.id !== removedItem)
     }
   }
 
@@ -37,7 +40,23 @@ export default {
 </script>
 
 <style>
-  *{
+  * {
+    padding: 0;
     margin: 10px;
+    box-sizing: border-box;
+    align-items: center;
+    justify-content: center;
+  }
+  body {
+    background: #889EAF;
+  }
+  .container {
+    padding: 30px;
+    max-width: 500px;
+    margin: 30px auto;
+    background: white;
+    border-radius: 10px;
+    border: 1px solid white;
+    box-shadow: 1px 1px 6px 1px rgba(0.3, 0.3, 0.1, 0.3);
   }
 </style>
