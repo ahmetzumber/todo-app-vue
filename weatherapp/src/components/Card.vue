0c6f5c260e65aad8660f194bde7072e01bc6e7f7
Ahmet <ahmetzumber5@gmail.com>
2021-09-26 14:49:57 +0300

    degree and day card added

---
+++ weatherapp/src/components/Card.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>{{ degree }}</h1>
+        <h2>{{ day }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "card",
+    props:{
+        degree: String,
+        day: String
+    }
+
+}
+</script>
+
+<style scoped>
+    div{
+        text-align: center;
+    }
+    h1{
+        font-size: 50px;
+    }
+
+</style>
